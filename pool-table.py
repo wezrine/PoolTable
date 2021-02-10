496e5cd5395acaa65f5714a0bf474123dacecef6
wezrine@me.com <williamezrine@williams-mbp.lan>
2021-02-10 16:56:25 -0500

    added start_time to occupied tables

--- pool-table.py
+++ pool-table.py
@@ -1,5 +1,8 @@
-tables = []
 from datetime import datetime
+tables = []
+fmt = '%H:%M:%S'
+
+
 
 class PoolTable:
     def __init__(self,number):
@@ -7,17 +10,30 @@ class PoolTable:
         self.is_occupied = False
         self.start_time = None
         self.end_time = None
-        self.total_time = None
+        self.time_played = None
 
-    def checkout(self):
+    def check_out(self):
         self.start_time = datetime.now()
         self.end_time = None
         self.is_occupied = True
     
-    def checkin(self):
+    def check_in(self):
         self.end_time = datetime.now()
         self.start_time = None
         self.is_occupied = False
+    
+    # def total_time(self):
+    #     self.time_played = self.end_time - self.start_time
+
+    # def time_clear(self):
+    #     self.start_time = None
+
+def format_time(dt):
+    if dt == None:
+        return "Time not started"
+    else:
+        return dt.strftime(fmt)
+
     
 def check_out_table():
     print("\nUNOCCUPIED TABLES:")
@@ -26,16 +42,17 @@ def check_out_table():
             print(f"Table {table.number}")
     ask = int(input("Choose a table: "))
     table = tables[ask-1]
-    table.checkout()
+    table.check_out()
 
 def check_in_table():
     print("\nOCCUPIED TABLES:")
+
     for table in tables:
         if table.is_occupied == True:
-            print(f"Table {table.number}")
+            print(f'Table {table.number} - start: {format_time(table.start_time)}')
     ask = int(input("Choose a table: "))
     table = tables[ask-1]
-    table.checkin()
+    table.check_in()
 
 # creates 12 tables
 for i in range (0,12):
