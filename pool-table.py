347b2adc77fe92d1b294eeb741fea39de50f1294
wezrine@me.com <williamezrine@Williams-MacBook-Pro.local>
2021-02-11 15:24:59 -0500

    working prototype - writes to .json & .txt

--- pool-table.py
+++ pool-table.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 tables = []
 fmt = '%H:%M:%S'
 
@@ -17,17 +18,10 @@ class PoolTable:
     
     def check_in(self):
         self.end_time = datetime.now()
-        self.start_time = None
         self.is_occupied = False
     
-    # def total_time(self):
-    #     self.time_played = self.end_time - self.start_time
-
-    # def total_time(self):
-    #     self.time_played = datetime.now() - self.start_time
-
-    # def time_clear(self):
-    #     self.start_time = None
+    def total_time(self):
+        return self.end_time - self.start_time
 
 def format_time(dt):
     if dt == None:
@@ -35,25 +29,24 @@ def format_time(dt):
     else:
         return dt.strftime(fmt)
 
-    
 def check_out_table():
     print("\nUNOCCUPIED TABLES:")
     for table in tables:
         if table.is_occupied == False:
             print(f"Table {table.number}")
-    ask = int(input("Choose a table: "))
+    ask = int(input("\nChoose a table: "))
     table = tables[ask-1]
     table.check_out()
 
 def check_in_table():
     print("\nOCCUPIED TABLES:")
-
     for table in tables:
         if table.is_occupied == True:
-            print(f"Table {table.number} | Start Time: {format_time(table.start_time)} | End Time: {format_time(table.end_time)} | Total Time Played: {format_time(table.time_played)}")
-    ask = int(input("Choose a table: "))
+            print(f"Table {table.number}")
+    ask = int(input("\nChoose a table: "))
     table = tables[ask-1]
     table.check_in()
+    print(f"Table {table.number} - Total Time: {table.total_time()}")
 
 # creates 12 tables
 for i in range (0,12):
@@ -63,10 +56,28 @@ for i in range (0,12):
 ## MENU ##
 while True:
     print("\nPOOL TABLE MANAGEMENT SYSTEM\n****************************\n")
-    exit = input("Press [1] to check out a table\nPress [2] to check in a table\n")
-    if exit == "1":
-        # Checkout a table
+    exit = input("Press [1] to check out a table\nPress [2] to check in a table\nPress any other key to quit\n\n")
+    if exit == "1": # Check out a table
         check_out_table()
-    elif exit == "2":
-        # Checkin a table
+    elif exit == "2": # Check in a table
         check_in_table()
+    else:
+        break
+
+# Writes to txt
+with open("11-22-2017.txt","w") as file:
+    for pt in tables:
+        table_info = f"#{pt.number} - START: {format_time(pt.start_time)} - END: {format_time(pt.end_time)}\n"
+        file.write(table_info)
+
+# Writes to JSON
+array = []
+with open("11-22-2017.json","w") as file_object:
+    for table in tables:
+        user = {"TableNo": table.number, "Start": format_time(table.start_time), "End": format_time(table.end_time)}
+        array.append(user)
+    json.dump(array,file_object)
+    
+
+
+
