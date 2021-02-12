747d573c96273cec526aa76bc5472fa87e22f3db
wezrine@me.com <williamezrine@williams-mbp.lan>
2021-02-12 16:17:20 -0500

    Final

--- pool-table.py
+++ pool-table.py
@@ -1,54 +1,51 @@
 from datetime import datetime
 import json
 tables = []
-fmt = '%S'
+fmt = '%H:%M:%S'
 from pool_table_class import PoolTable
 
 def format_time(dt):
     if dt == None:
-        return "Unoccupied"
+        return "N/A"
     else:
         return dt.strftime(fmt)
 
 def check_out_table():
     print("\nUNOCCUPIED TABLES:")
-    for table in tables:
+    for table in tables: # shows all unoccupied tables
         if table.is_occupied == False:
             print(f"Table {table.number}")
     ask = int(input("\nChoose a table to check out: "))
     table = tables[ask-1]
-    if table.is_occupied == True:
+    if table.is_occupied == True: # occupied tables cannot be checked out
         print(f"Pool Table {table.number} is currently occupied")
     else:
         table.check_out()
-        print(table.total_time())
 
 def check_in_table():
     print("\nOCCUPIED TABLES:")
-    for table in tables:
+    for table in tables: # shows all occupied tables
         if table.is_occupied == True:
             print(f"Table {table.number}")
     ask = int(input("\nChoose a table to check in: "))
     table = tables[ask-1]
-    if table.is_occupied == False:
+    if table.is_occupied == False: # unoccupied tables cannot be checked in
         print("Pool table was never occupied")
     else:
         table.check_in()
-        print(f"\nTable {table.number} has been checked in. Total Time: ")
-        print(format_time(table.start_time))
-        print(format_time(table.end_time))
-        print(table.total_time())
+        print(f"\nTable {table.number} has been checked in. Total Time: {table.time_played} Cost: $ {table.cost}")
 
 def view_all_tables():
     print("\nALL TABLES:")
     for table in tables:
         if table.is_occupied == False:
-            print(f"Table {table.number}")
+            print(f"Table {table.number} |")
         else:
-            # cost_of_table: unsupported operand type for * int and 
-            print(f"Table {table.number} is currently occupied")
+            table.view_tables()
+            print(f"Table {table.number} | Start: {table.start_time} | Total Time: {table.time_played} | Cost: $ {table.cost}")
+            table.clear()
 
-# creates 12 tables
+# Creates 12 tables
 for i in range (0,12):
     table = PoolTable(i+1)
     tables.append(table)
@@ -56,12 +53,13 @@ for i in range (0,12):
 ## MENU ##
 while True:
     print("\nPOOL TABLE MANAGEMENT SYSTEM\n****************************\n")
-    exit = input("Press [1] to check out a table\nPress [2] to check in a table\nPress [3] to view all tables\nPress any other key to quit\n\n")
+    exit = input("Press [1] to check out a table\nPress [2] to check in a table\n"
+    "Press [3] to view all tables\nPress any other key to quit\n\n")
     if exit == "1": # Check out a table
         check_out_table()
     elif exit == "2": # Check in a table
         check_in_table()
-    elif exit == "3":
+    elif exit == "3": # View all tables
         view_all_tables()
     else:
         break
@@ -69,13 +67,13 @@ while True:
 # Writes to txt
 with open("11-22-2017.txt","w") as file:
     for pt in tables:
-        table_info = f"#{pt.number} - START: {format_time(pt.start_time)} - END: {format_time(pt.end_time)}\n"
+        table_info = f"#{pt.number} | START: {format_time(pt.start_time)} | END: {format_time(pt.end_time)} | TIME PLAYED: {pt.time_played} | COST: {pt.cost}\n"
         file.write(table_info)
 
 # Writes to JSON
 array = []
 with open("11-22-2017.json","w") as file_object:
     for table in tables:
-        user = {"TableNo": table.number, "Start": format_time(table.start_time), "End": format_time(table.end_time)}
-        array.append(user)
+        table_dictionary = {"TableNo": table.number, "Start": format_time(table.start_time), "End": format_time(table.end_time), "Time Played": table.time_played, "Cost": table.cost}
+        array.append(table_dictionary)
     json.dump(array,file_object)
--- pool_table_class.py
+++ pool_table_class.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class PoolTable:
     def __init__(self,number):
         self.number = number
@@ -6,21 +7,29 @@ class PoolTable:
         self.start_time = None
         self.end_time = None
         self.time_played = None
-
-    def check_out(self):
-        self.start_time = datetime.now()
-        self.end_time = None
-        self.is_occupied = True
-    
-    def check_in(self):
-        self.end_time = datetime.now()
-        self.is_occupied = False
-    
-    def total_time(self):
-        return self.end_time - self.start_time
+        self.cost = 0.00
 
     def current_time(self):
-        return datetime() - self.start_time
-    
-    def cost_of_table(self):
-        return 30 * self.end_time - self.start_time
+        return datetime.now() - self.start_time
+
+    def check_out(self):
+        self.is_occupied = True
+        self.start_time = datetime.now()
+        self.end_time = None
+        self.cost = 1.00
+        
+    def check_in(self):
+        self.is_occupied = False
+        self.end_time = datetime.now()
+        self.time_played = self.current_time()
+        self.cost += 30*(self.current_time().total_seconds())/3600
+
+    def view_tables(self):
+        self.end_time = self.current_time()
+        self.time_played = self.current_time()
+        self.cost += 30*(self.current_time().total_seconds())/3600
+
+    def clear(self):
+        self.end_time = None
+        self.time_played = None
+        self.cost = 1.00
